e096c8a448e5e1451b23164b5c6f760533c17876
eastWillow <bobgash2@gmail.com>
2025-03-08 10:54:32 +0800

    Implement q_remove_tail for trace-02-ops
    Refer to q_remove_head
    End of the list is head->prev
    
    Change-Id: Iff49823e28e9417c8f7b3e463e9d97dab0743746

--- queue.c
+++ queue.c
@@ -71,7 +71,14 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if ((!head) || (!sp) || (bufsize == 0))
+        return NULL;
+    element_t *node = list_entry(head->prev, element_t, list);
+    if (!node->value)
+        return NULL;
+    strncpy(sp, node->value, bufsize);
+    list_del(head->prev);
+    return node;
 }
 
 /* Return number of elements in queue */
